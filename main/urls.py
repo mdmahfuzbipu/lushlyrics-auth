eacdaefa28adc72dc042c9e8271bbab70d85579c
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-06-30 21:07:52 +0600

    Add password reset urls using django auth_views

--- main/urls.py
+++ main/urls.py
@@ -6,11 +6,41 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path("", views.default, name='default'),
-    path("playlist/", views.playlist, name='your_playlists'),
-    path("search/", views.search, name='search_page'),
+    path("", views.default, name="default"),
+    path("playlist/", views.playlist, name="your_playlists"),
+    path("search/", views.search, name="search_page"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     
-    path("login/", views.CustomLoginView.as_view(), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name='logout'),
-    path("signup/", views.SignUpView.as_view(), name='signup'), 
+    
+    # password reset urls
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="auth/password_reset_form.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="auth/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="auth/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="auth/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
