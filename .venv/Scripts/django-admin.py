17aaf6b4ab5907933778f618325bd6e5d31a1fdc
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-06-30 16:05:47 +0600

    Update dependencies to latest versions

--- .venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!F:\LEARNING JOURNEY\MyProjects\Django Projects\Login System Coursera\lushlyrics-auth\.venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
