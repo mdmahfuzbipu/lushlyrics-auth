aa047c9d0d3c67c7bc059aca0625c345c9999167
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-06-30 15:58:47 +0600

    Update project structure

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\LEARNING JOURNEY\MyProjects\Django Projects\Login System Coursera\lushlyrics-auth\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
